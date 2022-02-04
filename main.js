b8c6414fe0a70b35a59eb3aed65fc8734141bfbf
xalita <brunofneferreira@gmail.com>
2022-02-04 22:45:15 +0000

    second

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // hello
+const messagesFactory = (id, message) => {
+    return {
+        id,
+        message
+    }
+};
+
+
+const messages = ['Hey','oi', 'batatas', 'cebolas','Amor','pinar é bom', 'sexo é melhor', 'fazer amor também','sara queres pinar?'];
+
+const random = () => messages [Math.floor (Math.random() * messages.length)];
+
+const arr = [];
+let id = 1;
+
+while (arr.length < 15) {
+    arr.push(messagesFactory(id ,random()));
+    id++;
+}
+
+console.log (arr);
